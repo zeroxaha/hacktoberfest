71013a8e054f724df253ee7252854560390e6049
Daffa Takarai <40546955+DaffaTakarai@users.noreply.github.com>
2021-10-01 11:00:21 +0700

    Add files via upload

---
+++ config.example.php
@@ -0,0 +1,9 @@
+<?php
+const BASE_URL = 'http://localhost:8080/';
+
+const DB_HOST = 'db:3306';
+const DB_NAME = 'kurteyki';
+const DB_USER = 'app';
+const DB_PASS = 'app';
+const DB_DRIVER = 'mysqli';
+?>
